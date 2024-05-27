cd89e213e3c5491234c8eb0cda865479c336d412
Asad Bukhari <asad@plastk.ca>
2024-05-27 12:35:43 +0500

    update

--- next.config.js
+++ next.config.js
@@ -12,6 +12,10 @@ const nextConfig = {
         protocol: 'http',
         hostname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cakeshares.s3.us-west-1.amazonaws.com',
+      },
     ],
   },
 };
