cfbdda699db4d4b165a55bc68aaec6ec60c688a9
Muhammad Uzair <uzair@webevis.com>
2024-05-28 17:26:26 +0500

    feat: completed invetment and product table

--- src/services/productService.js
+++ src/services/productService.js
@@ -46,6 +46,41 @@ const productService = {
     throw new Error(message ?? 'Something Went Wrong');
   },
 
+  GetAllInvestments(searchQuery, fetch) {
+    const [investments, setInvestments] = useState({
+      investments: [],
+      totalItems: 0,
+    });
+    const { cancellablePromise } = useCancellablePromise();
+    const [investmentStatus, setInvestmentStatus] = useState(STATUS.LOADING);
+    useEffect(() => {
+      setInvestmentStatus(STATUS.LOADING);
+      cancellablePromise(this.getAllInvestments(searchQuery))
+        .then(res => {
+          setInvestments(() => res);
+          setInvestmentStatus(STATUS.SUCCESS);
+        })
+        .catch(() => setInvestmentStatus(STATUS.ERROR));
+    }, [searchQuery, fetch]);
+    return {
+      investments_loading: investmentStatus === STATUS.LOADING,
+      investments_error: investmentStatus === STATUS.ERROR,
+      investments_data: investments,
+    };
+  },
+
+  async getAllInvestments({ page = 1, itemsPerPage = 10, searchText = '' }) {
+    let res = await Fetch.get(
+      `${this._url}/get-all-investments-super?page=${page}&itemsPerPage=${itemsPerPage}&searchText=${searchText}`,
+    );
+    if (res.status >= 200 && res.status < 300) {
+      res = await res.json();
+      return res;
+    }
+    const { message } = await res.json();
+    throw new Error(message ?? 'Something Went Wrong');
+  },
+
   async updateUser(userId, payload) {
     let res = await Fetch.upload(`${this._url}/update-user/${userId}`, 'PUT', payload);
     if (res.status >= 200 && res.status < 300) {
