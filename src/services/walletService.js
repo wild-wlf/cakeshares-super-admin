fb19d45015717467227ea4c496b61bb86e2871ec
Muhammad Uzair <uzair@webevis.com>
2024-05-29 18:47:17 +0500

    feat: did add balacen work

---
+++ src/services/walletService.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+import { useCancellablePromise } from '../helpers/promiseHandler';
+import { Fetch } from '../helpers/fetchWrapper';
+
+const STATUS = {
+  LOADING: 'loading',
+  SUCCESS: 'success',
+  ERROR: 'error',
+};
+
+const walletService = {
+  _url: `${process.env.NEXT_PUBLIC_USER_URL}`,
+
+  async addBalance(payload) {
+    let res = await Fetch.post(`${this._url}/add-balance/`, payload);
+    if (res.status >= 200 && res.status < 300) {
+      res = await res.json();
+      return res;
+    }
+    const { message } = await res.json();
+    throw new Error(message ?? 'Something went wrong');
+  },
+};
+
+export default walletService;
