78f529d4b0da812e2dda334d68c76b2077e3ce65
asdbukhari <asadbukhari9895@gmail.com>
2024-05-31 02:27:25 +0500

    socket context

--- src/components/molecules/Notifications/index.jsx
+++ src/components/molecules/Notifications/index.jsx
@@ -1,41 +1,42 @@
-import React from "react";
-import { ImageHolder, NotificationsHolder } from "./Notifications.styles";
-import Investment from "../../../../public/assets/investment-icon.svg";
-import Property from "../../../../public/assets/property-icon.svg";
-import Message from "../../../../public/assets/message-icon.svg";
-import Image from "next/image";
+import React from 'react';
+import { ImageHolder, NotificationsHolder } from './Notifications.styles';
+import Investment from '../../../../public/assets/investment-icon.svg';
+import Property from '../../../../public/assets/property-icon.svg';
+import Message from '../../../../public/assets/message-icon.svg';
+import Image from 'next/image';
 
 const Notifications = () => {
   const notificationData = [
     {
       image: Message,
-      heading: "1 New Message",
-      date: "19 Mar, 2024 | 20:50 PM",
-      tag: "New",
-      text: "Hey Man! I am interested in your Asset, wanna have conversation?",
-      background: "rgba(78, 97, 153, 0.2)",
+      heading: '1 New Message',
+      date: '19 Mar, 2024 | 20:50 PM',
+      tag: 'New',
+      text: 'Hey Man! I am interested in your Asset, wanna have conversation?',
+      background: 'rgba(78, 97, 153, 0.2)',
     },
     {
       image: Investment,
-      heading: "New Open Investment",
-      tag: "New",
-      date: "19 Mar, 2024 | 20:50 PM",
-      background: "rgba(78, 97, 153, 0.2)",
+      heading: 'New Open Investment',
+      tag: 'New',
+      date: '19 Mar, 2024 | 20:50 PM',
+      background: 'rgba(78, 97, 153, 0.2)',
     },
     {
       image: Property,
-      heading: "Egypt Property Product Update",
-      date: "19 Mar, 2024 | 20:50 PM",
+      heading: 'Egypt Property Product Update',
+      date: '19 Mar, 2024 | 20:50 PM',
       text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's",
-      background: "rgba(64, 143, 140, 0.2)",
+      background: 'rgba(64, 143, 140, 0.2)',
     },
     {
       image: Investment,
-      heading: "New Open Investment",
-      date: "19 Mar, 2024 | 20:50 PM",
-      background: "rgba(78, 97, 153, 0.2)",
+      heading: 'New Open Investment',
+      date: '19 Mar, 2024 | 20:50 PM',
+      background: 'rgba(78, 97, 153, 0.2)',
     },
   ];
+  // !TODO add notification logic here
   return (
     <NotificationsHolder>
       {notificationData.map((item, index) => (
