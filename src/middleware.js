17232671e6ca14cbf219d2de579204b2bb29d73b
Muhammad Uzair <uzair@webevis.com>
2024-05-30 13:55:31 +0500

    feat: fixed middleware

--- src/middleware.js
+++ src/middleware.js
@@ -44,5 +44,5 @@ export function middleware(request) {
     }
     return NextResponse.redirect(`${currentOrigin}/`);
   }
-  // return NextResponse.redirect(`${currentOrigin}/sign-in`);
+  return NextResponse.next();
 }
